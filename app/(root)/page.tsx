0d2400ec74a4aeb73ad89d8e6c0639586c267344
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-03 10:26:33 +0530

    feat: root

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex max-h-[100vh] w-full p-5">
+      <h1 className="text-[25px] font-outfit ">Vind</h1>
+    </div>
+  );
+};
+
+export default page;
