29ad44bdd3f8e41e640c9d7be38025adcdb29df3
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-04 10:49:09 +0530

    feat:  Input to add the files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,8 +7,8 @@ const page = () => {
       <h1 className="text-[25px] font-outfit ">Vind</h1>
     </div>
     <div className="flex flex-row h-[90vh]" >
-      <div className="w-[15%]" >
-        <h1 className="m-5">Documents</h1>
+      <div className="w-[15%] flex flex-col items-center  " >
+        <input type="text" className="w-[90%] mt-5 p-1 border rounded-[10px] " placeholder="name of the file" />
       </div>
       <div className="flex  w-[65%] border-r border-l" style={{ background: '#131313', borderColor: '#252525' }}>
         <textarea className="w-full p-10 outline-none resize-none " name="document" id="text-area"></textarea>
