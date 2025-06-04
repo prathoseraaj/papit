7d52283b0ea44d59ac402821164a9b772f7d6ecf
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-04 10:57:14 +0530

    fix: alginment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,20 +4,27 @@ import React from "react";
 const page = () => {
   return (
     <>
-    <div className="flex max-h-[100vh] w-full p-2 pl-5 border-b" style={{borderColor:'#252525'}}>
-      <h1 className="text-[25px] font-outfit ">Vind</h1>
-    </div>
-    <div className="flex flex-row h-[90vh]" >
-      <div className="w-[15%] flex flex-col items-center  " >
-        <input type="text" className="w-[90%] mt-5 p-1 border rounded-[10px] " placeholder="name of the file" />
+      <div
+        className="flex max-h-[100vh] w-full p-2 pl-5 border-b"
+        style={{ borderColor: "#252525" }}
+      >
+        <h1 className="text-[25px] font-outfit ">Vind</h1>
       </div>
-      <div className=" flex w-[65%]">
-        <InputField/>
+      <div className="flex flex-row h-[90vh]">
+        <div className="w-[15%] flex flex-col items-center  ">
+          <input
+            type="text"
+            className="w-[90%] mt-5 p-1 border border-gray-600 outline-none rounded-[10px] "
+            placeholder="name of the file"
+          />
+        </div>
+        <div className=" flex w-[65%]">
+          <InputField />
+        </div>
+        <div className="w-[20%]">
+          <h1 className="m-5">Commit message</h1>
+        </div>
       </div>
-      <div className="w-[20%]">
-        <h1 className="m-5">Commit message</h1>
-      </div>
-    </div>
     </>
   );
 };
