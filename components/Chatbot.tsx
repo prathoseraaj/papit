cfed2b44b652512d3fef4b738e7a557e4c7538af
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-06 16:22:00 +0530

    feat: gemini logo

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Chatbot = () => {
   return (
-    <div>Chatbot</div>
-  )
-}
+    <div className="flex group items-center m-5 flex-row relative group ">
+      <div className="relative cursor-pointer pointer-events-auto ">
+        <Image src="/image.png" alt="gemini logo" height={25} width={25} />
+      </div>
+      <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-1000">
+        <h1>Gemini</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Chatbot
+export default Chatbot;
