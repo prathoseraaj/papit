fac886eb619c4389a33084e72e6ad7e39b27b3c6
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-07 11:33:13 +0530

    feat: basic structure of chatbot

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -3,12 +3,23 @@ import React from "react";
 
 const Chatbot = () => {
   return (
-    <div className="flex group items-center m-5 flex-row relative group ">
-      <div className="relative cursor-pointer pointer-events-auto ">
-        <Image src="/image.png" alt="gemini logo" height={25} width={25} />
+    <div>
+      {/* Chatbot Name */}
+      <div className="flex group items-center m-5 flex-row relative group ">
+        <div className="relative cursor-pointer pointer-events-auto ">
+          <Image src="/image.png" alt="gemini logo" height={25} width={25} />
+        </div>
+        <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-1000 erase-out">
+          <h1>BroDoc</h1>
+        </div>
       </div>
-      <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-1000">
-        <h1>BroDoc</h1>
+      {/* Chat Window */}
+      <div>
+
+      </div>
+      {/* Chatbot Input */}
+      <div>
+
       </div>
     </div>
   );
