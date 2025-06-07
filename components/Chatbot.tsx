6ce9e87f8ee867462fd3922ad5635e3ce95c26d3
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-07 12:46:25 +0530

    fix: input div

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import React from "react";
+import { LuSendHorizontal } from "react-icons/lu";
 
 const Chatbot = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       {/* Chatbot Name */}
-      <div className="flex group items-center m-5 flex-row relative group ">
+      <div className="flex group items-center m-5 flex-row h-[25px] relative group ">
         <div className="relative cursor-pointer pointer-events-auto ">
           <Image src="/image.png" alt="gemini logo" height={25} width={25} />
         </div>
@@ -14,12 +15,21 @@ const Chatbot = () => {
         </div>
       </div>
       {/* Chat Window */}
-      <div>
-
-      </div>
+      <div className="h-[75vh]"></div>
       {/* Chatbot Input */}
-      <div>
-
+      <div
+        className="flex items-center px-4 py-2 border-t"
+        style={{ borderColor: "#252525" }}
+      >
+        <div className="flex items-center w-full relative">
+          <input
+            type="text"
+            className="w-full pl-4 pr-10 py-2 rounded-md bg-[#131313] text-white border outline-none"
+            style={{ borderColor: "#252525" }}
+            placeholder="What do you want?"
+          />
+          <LuSendHorizontal className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-300 hover:text-white" />
+        </div>
       </div>
     </div>
   );
