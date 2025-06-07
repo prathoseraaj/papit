680f850bab8daccbc548117b588e8e6c041993d5
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-07 14:09:38 +0530

    fix: route api typo

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.gemini_api_url}`;
 
 export async function POST(req) {
   try {
