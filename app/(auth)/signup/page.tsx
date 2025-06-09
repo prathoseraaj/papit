95981696fb6380f263978ba294399e506b28402f
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-09 16:14:46 +0530

    feat: Add login and signup functionality

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='h-screen  w-full flex flex-col'>
+      <div className='flex flex-row h-[90%] w-full'>
+        <div className='w-[60%] flex justify-center items-center'>
+          Vind
+        </div>
+        <div className='w-[40%] flex justify-center bg-blue-50 items-center'>
+          form code
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
