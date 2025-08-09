c67ef266c33e2a49d775c58ef5b1891e5525a77c
prathoseraaj <prathoseraaj0312@gmail.com>
2025-08-09 20:54:39 +0530

    fix: supabase url

--- libs/supabaseClient.ts
+++ libs/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl: string | undefined = process.env.supabase_url;
-const supabaseAnonKey: string | undefined = process.env.supabase_anon_key;
+const supabaseUrl: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables. Check NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY.');
