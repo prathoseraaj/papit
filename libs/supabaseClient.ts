d0a2b607f4db5c4d7f38bbf223bdd4db6193761b
prathoseraaj <prathoseraaj0312@gmail.com>
2025-08-09 21:04:35 +0530

    fix: client issues

--- libs/supabaseClient.ts
+++ libs/supabaseClient.ts
@@ -6,5 +6,5 @@ const supabaseAnonKey: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_ANO
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables. Check NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY.');
 }
-
+  
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
